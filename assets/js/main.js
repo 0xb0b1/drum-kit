a7975cb98ea61de98729f6e19b5df2042af2c61d
0xb0b1 <vcente82@gmail.com>
2020-11-06 10:44:59 -0300

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,23 @@
-window.addEventListener("keydown", function(e) {
+
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
     if(!audio) return // stops the function from running
+    audio.currentTime = 0 // rewind to the start
+    audio.play()
 
-    console.log(audio)
-})
+    key.classList.add('playing')
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return // skip it if it's not a transform
+
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener("keydown", playSound)
